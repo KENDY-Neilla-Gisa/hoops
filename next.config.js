7d3136bdc934ce904d0986ed995ebf77aeb3e9a0
KENDY-Neilla-Gisa <kendyneilla@gmail.com>
2025-10-31 15:05:47 +0200

    chore: update favicon configuration and references

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    suppressDeprecationWarnings: true
+  },
   eslint: {
     dirs: ['src'],
     ignoreDuringBuilds: true, // Skip ESLint during build
